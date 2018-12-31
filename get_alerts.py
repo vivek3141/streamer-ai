08316bb76cf2db130fee7b23a9f65fd03e730666
Vivek Verma <vivnps.verma@gmail.com>
2018-12-30 19:42:32 -0800

    Updating Things

--- get_alerts.py
+++ get_alerts.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
+from pytesseract import image_to_string
+from PIL import Image
+
 import cv2
 import numpy as np
 import time
+import random
+import pyttsx3
+
 config = {i.split(":")[0]: i.split(":")[1][:-1] for i in open("config").readlines()}
 URL = f"https://streamlabs.com/alert-box/v3/{config['access_token']}"
 DRIVER = 'chromedriver'
 driver = webdriver.Chrome(DRIVER)
+driver.get(URL)
+
+while True:
+    driver.save_screenshot("shot.png")
+    img = cv2.imread("shot.png")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print(image_to_string(gray))
+    time.sleep(0)
