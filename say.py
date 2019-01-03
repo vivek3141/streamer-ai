ccdc23c9d337a28f2ca5a129efe702a14d265ac1
Vivek Verma <vivnps.verma@gmail.com>
2019-01-02 21:28:31 -0800

    Adding Subsriber

--- say.py
+++ say.py
@@ -2,9 +2,15 @@ import urllib.request
 import json
 
 config = {i.split(":")[0].strip(): i.split(":")[1].strip() for i in open("config").readlines()}
+
 URL = "https://api.twitch.tv/kraken/channels/" + config['Channel'] + "/follows"
+SUB = "https://api.twitch.tv/kraken/channels/" + config['Channel'] + "/subscribers"
+
 REQUEST = urllib.request.Request(URL)
 REQUEST.add_header("Client-ID", config['Client-ID'])
+
+SUBR = urllib.request.Request(SUB)
+SUBR.add_header("Client-ID", config['Client-ID'])
 
 
 def get_last_follower():
@@ -14,10 +20,18 @@ def get_last_follower():
     return j['follows'][0]['user']['display_name']
 
 
-lastFollower = get_last_follower()
+def get_subscribers():
+    resp = urllib.request.urlopen(REQUEST)
+    data = resp.read().decode('utf-8')
+    j = json.loads(data)
+    return j
 
+
+print(get_subscribers())
+"""
+lastFollower = get_last_follower()
 while True:
     follower = get_last_follower()
     if follower != lastFollower:
         print(f"{follower} has just followed!")
-        lastFollower = follower
+        lastFollower = follower"""
