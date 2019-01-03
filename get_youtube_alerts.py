6467946b098a7507ed8fbf565d9424b7a4f833ab
Vivek Verma <vivnps.verma@gmail.com>
2019-01-03 14:18:07 -0800

    Adding Youtube Alerts

--- get_youtube_alerts.py
+++ get_youtube_alerts.py
@@ -1,4 +1,25 @@
 import json
+import socketio
 
 socket_token = json.load(open("socket_token.json"))['socket_token']
-print(socket_token)
+URL = f"https://sockets.streamlabs.com?token={socket_token}"
+sio = socketio.Client()
+
+
+@sio.on('connect')
+def connect():
+    print('Connected')
+
+
+@sio.on('event')
+def event(data):
+    print('I received a message!')
+    print(data)
+
+
+sio.connect(URL)
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    pass
