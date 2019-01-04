650a6360d824ea797b775779de5951f3fb43bc63
Vivek Verma <vivnps.verma@gmail.com>
2019-01-04 11:49:07 -0800

    Updating Things

--- src/main.py
+++
@@ -1,4 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-
--- src/streamlabs.py
+++
@@ -1,7 +0,0 @@
-import json
-import requests
-
-access = json.load(open("access_token.json"))
-print(access)
-r = requests.request("GET", 'https://streamlabs.com/api/v1.0/socket/token', params={"access_token":access['access_token']})
-print(r.text)
