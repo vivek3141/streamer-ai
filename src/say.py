476c5e7a0d5029918ee7375d46dca1d1e14d77e0
Vivek Verma <vivnps.verma@gmail.com>
2019-01-06 00:40:18 -0800

    Cleaning Things

--- src/say.py
+++ src/say.py
@@ -1,8 +1,6 @@
 from gtts import gTTS
 import os
-from time import sleep
-import os
-import pyglet
+
 
 tts = gTTS(text='Hello World', lang='en')
 filename = '/tmp/temp.mp3'
@@ -21,11 +19,4 @@ while True:
     tts = gTTS(to_say)
     tts.save(filename)
 
-    #music = pyglet.media.load(filename, streaming=False)
-    #music.play()
-
-    #sleep(music.duration)
-    #os.remove(filename)
     os.system(f"mpg123 {filename}")
-    #engine.say(to_say)
-    #engine.runAndWait()
